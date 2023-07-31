e666ba19fb374a98cdbefcf9ebfae27d14e0cac1
Mayank.Agrawal <mayank.agrawal@procogia.com>
2023-07-31 15:09:12 +0530

    add comments to google charts application

--- shiny-examples/182-google-charts/app.R
+++ shiny-examples/182-google-charts/app.R
@@ -2,13 +2,17 @@
 #   https://github.com/jcheng5/googleCharts
 # Install:
 #   devtools::install_github("jcheng5/googleCharts")
+
+# load libraries ###############################################################
 library(googleCharts)
 library(shiny)
 library(dplyr)
 
+# load data ####################################################################
 data <- readRDS("healthexp.Rds")
 data$Region <- as.factor(data$Region)
 
+# compute x limits and y limits ----
 # Use global max/min for axes so the view window stays
 # constant as the user moves between years
 xlim <- list(
@@ -20,21 +24,24 @@ ylim <- list(
   max = max(data$Life.Expectancy) + 3
 )
 
+# ui ###########################################################################
 ui <- fluidPage(
-  # This line loads the Google Charts JS library
+  ## load the Google Charts JS library ----
   googleChartsInit(),
 
-  # Use the Google webfont "Source Sans Pro"
+  ## Use the Google webfont "Source Sans Pro" ----
   tags$link(
-    href=paste0("http://fonts.googleapis.com/css?",
+  href=paste0("http://fonts.googleapis.com/css?",
                 "family=Source+Sans+Pro:300,600,300italic"),
-    rel="stylesheet", type="text/css"),
+  rel="stylesheet", type="text/css"),
   tags$style(type="text/css",
-    "body {font-family: 'Source Sans Pro'}"
+             "body {font-family: 'Source Sans Pro'}"
   ),
-
+  
+  ## Add app header ----
   h2("Google Charts demo"),
 
+  ## Bubble plot div ----
   googleBubbleChart("chart",
     width="100%", height = "475px",
     # Set the default options for this chart; they can be
@@ -44,7 +51,7 @@ ui <- fluidPage(
     options = list(
       fontName = "Source Sans Pro",
       fontSize = 13,
-      # Set axis labels and ranges
+      ### Set axis labels and ranges ----
       hAxis = list(
         title = "Health expenditure, per capita ($USD)",
         viewWindow = xlim
@@ -53,22 +60,22 @@ ui <- fluidPage(
         title = "Life expectancy (years)",
         viewWindow = ylim
       ),
-      # The default padding is a little too spaced out
+      ### The default padding is a little too spaced out ----
       chartArea = list(
         top = 50, left = 75,
         height = "75%", width = "75%"
       ),
-      # Allow pan/zoom
+      ### Allow pan/zoom ----
       explorer = list(),
-      # Set bubble visual props
+      ### Set bubble visual props ----
       bubble = list(
         opacity = 0.4, stroke = "none",
-        # Hide bubble label
+        #### Hide bubble label ----
         textStyle = list(
           color = "none"
         )
       ),
-      # Set fonts
+      ### Set fonts ----
       titleTextStyle = list(
         fontSize = 16
       ),
@@ -79,6 +86,8 @@ ui <- fluidPage(
       )
     )
   ),
+  
+  ## Year Selector Widget ----
   fluidRow(
     shiny::column(4, offset = 4,
       sliderInput("year", "Year",
@@ -88,9 +97,10 @@ ui <- fluidPage(
   )
 )
 
-
+# server ###############################################################
 server <- function(input, output, session) {
 
+  ## Default color setup ----
   # Provide explicit colors for regions, so they don't get recoded when the
   # different series happen to be ordered differently from year to year.
   # http://andrewgelman.com/2014/09/11/mysterious-shiny-things/
@@ -100,6 +110,7 @@ server <- function(input, output, session) {
     names = levels(data$Region)
   )
 
+  ## reactive dataset ----
   yearData <- reactive({
     # Filter to the desired year, and put the columns
     # in the order that Google's Bubble Chart expects
@@ -113,6 +124,7 @@ server <- function(input, output, session) {
       arrange(Region)
   })
 
+  ## render bubble plot ----
   output$chart <- reactive({
     # Return the data and options
     list(
@@ -127,4 +139,9 @@ server <- function(input, output, session) {
   })
 }
 
-shinyApp(ui = ui, server = server)
+# run app ######################################################################
+# classic
+# shinyApp(ui = ui, server = server)
+
+# run with code view
+shinyApp(ui = ui, server = server, options = list(display.mode = "showcase")) # “auto”, “normal”, “showcase”
